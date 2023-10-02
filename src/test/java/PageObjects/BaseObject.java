6b6ab4f5bcde6a58af7edfd25ca45c4562f637fd
chandrima <chandrima353@gmail.com>
2023-10-02 22:01:03 +0530

    first commit

---
+++ src/test/java/PageObjects/BaseObject.java
@@ -0,0 +1,17 @@
+package PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BaseObject {
+	 WebDriver driver;
+	    
+	   public BaseObject(WebDriver driver)
+	     {
+		     this.driver=driver;
+		     PageFactory.initElements(driver,this);
+	     }
+	   	   
+}
+
+
